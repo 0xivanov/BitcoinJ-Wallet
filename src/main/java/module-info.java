f1d9189ac4dbd0521729791b4a601c638b8cfbf3
Ivan Ivanov <i.ivanov@seeburger.de>
2022-05-13 15:08:52 +0300

    Initial setup

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.ivanov.btcwallet {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.ivanov.btcwallet to javafx.fxml;
+    exports com.ivanov.btcwallet;
+}
